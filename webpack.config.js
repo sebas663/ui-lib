14582b78da77c5f83790a0c286a4b5ba95f4f3b5
sebas663 <sebastian_escuderoh@hotmail.com>
2022-02-02 20:36:36 -0300

    Se sube proyecto

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const nodeExternals = require('webpack-node-externals');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    library: '@sebas663/libreria',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    publicPath: '/dist/', 
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.(ts|tsx|js)$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: [
+          'style-loader', 
+          'css-loader',
+          'sass-loader',
+        ],
+        test: /\.(css|scss|sass)$/,
+        include: path.resolve(__dirname, './src'),
+      },
+      {
+        type: 'asset',
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json'],
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+};
+
+
