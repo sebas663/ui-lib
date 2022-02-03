7d8a39b8bc69d84a1f69a052fd14a3456718cd77
sebas663 <sebastian_escuderoh@hotmail.com>
2022-02-02 22:55:49 -0300

    Se agrega source map

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  devtool: 'source-map',
   externals: [nodeExternals()],
   output: {
     path: path.resolve(__dirname, 'dist'),
